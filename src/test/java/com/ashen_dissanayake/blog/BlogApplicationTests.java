38bf93e521a534cad15d2ae5a5fe853d4f1e04b9
Ashen-Shaminda <developer.ashen@gmail.com>
2025-05-16 15:57:39 +0530

    Initial commit.

---
+++ src/test/java/com/ashen_dissanayake/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashen_dissanayake.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
